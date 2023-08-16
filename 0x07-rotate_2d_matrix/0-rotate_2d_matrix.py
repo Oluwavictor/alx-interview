cde1a088a6d6be449f018fca3b8b3d082825dc17
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-08-16 13:57:57 +0100

    added a readme and a rotate_2d file

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""A module that rotates a 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """given a square matrix, return a 90 degrees rotated matrix"""
+
+    test_matrix = matrix.copy()
+    matrix.clear()
+
+    test_matrix.reverse()
+
+    for idx in range(len(test_matrix)):
+        new_row = [element[idx] for element in test_matrix]
+        matrix.append(new_row)
